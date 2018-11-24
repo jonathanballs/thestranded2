4a883c90c113128290b2521f50fd2f1165d3682c
Jonathan Balls <jonathanballs@protonmail.com>
2018-11-24 15:23:36 +0000

    Backend join room

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,9 @@ import { createServer, Server } from 'http';
 import * as path from 'path';
 import SocketIO from 'socket.io';
 import express from 'express';
+import joi from 'joi';
+
+import { GameRoom, Map, Player } from './gamestate';
 
 // Create server
 const app = express();
@@ -28,7 +31,51 @@ server.listen(port, () => {
     console.log("Stranded server started on port " + port);
 });
 
+const rooms: GameRoom[] = [];
+
 // Handle socket.io connections
-socket.on('connection', function(socket) {
-  console.log(`Connection recieved from ${socket.id}`);
+socket.on('connection', function (socket) {
+    console.log(`Connection recieved from ${socket.id}`);
+
+    socket.on('joinRoom', (userDetailsRaw) => {
+        const schema = joi.object().keys({
+            name: joi.string().alphanum().max(15).required(),
+            mode: joi.string().only(['spectator', 'player']).required(),
+            roomName: joi.string().optional().default('main'),
+            characterSpriteId: joi.string().optional(),
+        });
+
+        // Validate the input
+        joi.validate(userDetailsRaw, schema).then((userDetails) => {
+            if (userDetails.mode == 'spectator') {
+                socket.emit('serverError', 'Spectator mode not supported');
+                return;
+            }
+
+            // Create room if doesn't exist
+            if (rooms[userDetails.roomName] == undefined) {
+                const { roomName }  = userDetails;
+                socket.emit('joinRoom', { status: `Creating room ${roomName}`});
+                rooms[roomName] = new GameRoom(roomName);
+            }
+
+            // Create player and add to room
+            const p = new Player(userDetails.name, userDetails.characterSpriteId);
+            rooms[userDetails.roomName].addPlayer(p);
+
+            // Save user details to the socket object
+            this.userId = p.id;
+            this.roomName = userDetails.roomName;
+
+            socket.emit('joinRoom', {
+                player: p,
+                room: {
+                    name: this.roomName,
+                    seed: rooms[this.roomName].seed,
+                }
+            });
+        }).catch((err) => {
+            socket.emit('serverError', `Bad input to joinRoom: ${userDetailsRaw}: ${err}`);
+        })
+    })
 });
---
+++ backend/gamestate.ts
@@ -0,0 +1,64 @@
+// Game state data strucutres
+
+export class GameRoom {
+    name: string;
+    players: { [id: string]: Player }; // Lookup players by id
+    map: Map;
+    seed: number;
+
+    // Add a new player to the game
+    addPlayer(player: Player) {
+        // Find the first available id
+        const uNameAlpha = player.name.replace(/\W/g, '')
+        let i = 0;
+        while (this.players[`player-${uNameAlpha}-${i.toString()}`] == undefined) {
+            i++;
+        }
+
+        player.id = `player-${uNameAlpha}-${i.toString()}`;
+        this.players[player.id] = player;
+
+        // TODO: Set their position etc.
+    }
+
+    constructor(roomName: string) {
+        this.name = roomName;
+        // 100M possible seeds
+        this.seed = Math.floor(Math.random() * 100000000);
+    }
+}
+
+export class Map {
+}
+
+// Generic base class for "living things"; Zombies, players
+// and any other autonomous entity will subclass this.
+export class LivingEntity {
+    id: string;
+    type: string; // player/zombie etc.
+
+    constructor(type: string) {
+        this.type = type;
+    }
+
+    pos: {
+        x: number,
+        y: number,
+    }
+    rotation: number;
+    currentSpeed: number;
+    maxSpeed: number;
+}
+
+export class Player extends LivingEntity {
+    name: string;
+    characterSpriteId: string;
+    active: boolean = true;
+
+    constructor(name: string, characterSpriteId: string) {
+        super('player');
+        this.name = name;
+        // TODO: Check if a valid sprite id
+        this.characterSpriteId = characterSpriteId || 'default';
+    }
+}
