92e95350088e450d2c2e9b2a71dffeb32e36e9d0
Pedro Mendonca <pedrom@sesame.local>
2018-11-24 17:00:58 +0000

    Add camera movement closes #4

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import 'p5'
 import Sprite from './Sprite'
 import Player from './Player'
 import Anim from './Animation'
-import {CANVAS_SIZE} from './utils'
+import {DEBUG, CANVAS_SIZE, TILE_SIZE} from './utils'
 
 const [width, height] = CANVAS_SIZE
 var playerAnim:Anim;
@@ -22,13 +22,30 @@ const sketch = (s:any) => {
         player.x = 5
         player.y = 5
         lastUpdate = Date.now()
+        // s.translate(width/2, height/2)
     }
     s.draw = () => {
+        s.push()
+        s.stroke(255, 0, 0)
+        s.noFill()
+        s.ellipse(0,0, 50)
         const curTime = Date.now()
         s.background(255)
+        s.translate(
+            width/2 - (player.x * TILE_SIZE),
+            height/2 - (player.y * TILE_SIZE)
+            // (player.x * TILE_SIZE) + width/2,
+            // (player.y * TILE_SIZE) + height/2
+        )
         player.update(curTime - lastUpdate, s)
         player.draw(s)
         lastUpdate = curTime
+        if(DEBUG) {
+            s.fill('black')
+            s.stroke('red')
+            s.ellipse(0,0, 10)
+        }
+        s.pop()
     }
 }
 
