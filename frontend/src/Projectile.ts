746925649af748cd56abe4ed3183561223615718
Pedro Mendonca <pedrom@sesame.local>
2018-11-24 19:12:43 +0000

    Add basic projectile class

---
+++ frontend/src/Projectile.ts
@@ -0,0 +1,40 @@
+import 'p5'
+import Sprite from './Sprite'
+import { TILE_SIZE, DEBUG } from './utils'
+
+const LIFE_TIME = 5000
+const MAX_VEL = 1
+export default class Projectile extends Sprite{
+    alive: boolean
+    creationTime: number
+    constructor(look: p5.Image, x:number, y:number, rot:number) {
+        super(look, x, y, 10,10)
+        this.rot = rot
+        this.velX = MAX_VEL * Math.cos(rot)
+        this.velY = MAX_VEL * Math.sin(rot)
+        this.alive = true
+        this.creationTime = Date.now()
+    }
+
+    update(timeDelta: number, s:any) {
+        if(Date.now() - this.creationTime > LIFE_TIME) {
+            this.alive = false
+            return
+        }
+        const mult = timeDelta/100
+        this.x += this.velX * mult
+        this.y += this.velY * mult
+    }
+
+    draw(s:any) {
+        s.push()
+        s.translate(this.x * TILE_SIZE, this.y * TILE_SIZE)
+        s.ellipsis(0,0,10)
+        if (DEBUG) {
+            s.stroke(255, 0, 0)
+            s.noFill()
+            s.ellipse(0,0, this.width)
+        }
+        s.pop()
+    }
+}
